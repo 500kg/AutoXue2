3935aefc438f14d1d9c19a3224ff80209aa6300b
500kg <2426958134@qq.com>
2020-12-23 20:32:37 +0800

    2020/12/23

---
+++ xuexi/auto_xue.bat
@@ -0,0 +1,12 @@
+echo AutoXue is going to run in 60 seconds, if you don't need it, close this cmd.
+@echo off
+timeout 60
+
+cd C:\Github\AutoXue2
+
+
+start C:\LeiDian\LDPlayer4.0\dnplayer.exe
+
+timeout 120
+
+
